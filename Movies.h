5113253fbc0d4f4e90373514a4a455a3e5ba815d
RMU31 <rachana.mudrakolla@sky.uk>
2023-05-15 15:07:11 +0100

    add Movies list

---
+++ Movies.h
@@ -0,0 +1,47 @@
+#include <string>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+using namespace std;
+using namespace std::chrono;
+
+static string filterBy;
+
+// Defining the movies class
+class Movies {
+public:
+    map<string, string> data;
+    Movies *next;
+
+    ~Movies() {}  // No idea what this does
+
+    // Movies constructor
+    Movies(string rank, string title, string year, string score, string director, string cast, string consensus) {
+        data["rank"] = rank;
+        data["title"] = title;
+        data["year"] = year;
+        data["score"] = score;
+        data["director"] = director;
+        data["cast"] = cast;
+        data["consensus"] = consensus;
+
+    }
+
+    // This method will display the categories available to the user to filter the dataset by.
+    void Display() {
+        cout << "Rank: " << data["rank"] << endl;
+        cout << "Movie Title: " << data["title"] << endl;
+        cout << "Release Year: " << data["year"] << endl;
+        cout << "Critics Score /100: " << data["score"] << endl;
+        cout << "Director: " << data["director"] << endl;
+        cout << "Cast: " << data["cast"] << endl;
+        cout << "Critics Consensus: " << data["consensus"] << endl;
+    }
+
+    string GetData() {
+        return data[filterBy];
+    }
+};
---
+++ MoviesList.h
@@ -0,0 +1,28 @@
+#include "Movies.h"
+
+class MoviesList {
+public:
+    Movies *first;
+    int size;
+
+    MoviesList() {
+        first= 0;
+        size = 0;
+    }
+
+    void Insert(Movies *newMovies) {
+        newMovies->next = first;
+        first = newMovies;
+    }
+
+    void Display() {
+        Movies *current = first;
+        int count = 0;  // do I need this???
+
+        while(current != 0) {
+            current->Display();
+            current = current->next;
+            count++;
+        }
+    }
+};
